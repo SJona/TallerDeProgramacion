5cc0bd605431702dc5df520eb694aaf0a232ab3f
Daniel Adrián Grinman <dan.gr04@live.com>
2015-11-06 11:25:32 -0300

    por la mitad el 5

---
+++ Ejercicio 1/Ejercicio5/IRepositorioUsuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    public interface IRepositorioUsuario
+    {
+        void Agregar(Usuario pUsuario);
+
+        void Actualizar(Usuario pUsuario);
+
+        void Eliminar(string pCodigo);
+
+        IList<Usuario> ObtenerTodos();
+
+        Usuario ObtenerPorCodigo(string pCodigo);
+
+        IList<Usuario> ObtenerOrdenadosPor(IComparer<Usuario> pComparation);
+    }
+}
---
+++ Ejercicio 1/Ejercicio5/Imprementacion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    class Implementacion : IRepositorioUsuario
+    {
+        Usuario iUsuario;
+        IDictionary<string, Usuario> usuarios;
+
+        public Implementacion(Usuario pUsuario) {
+            usuarios.Add(pUsuario.Codigo,pUsuario);
+        }
+
+        public void Actualizar(Usuario pUsuario)
+        {
+            if (usuarios.ContainsKey(pUsuario.Codigo)) {
+
+            }
+        }
+
+        public void Agregar(Usuario pUsuario)
+        {
+            
+        }
+
+        public void Eliminar(string pCodigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Usuario> ObtenerOrdenadosPor(IComparer<Usuario> pComparation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario ObtenerPorCodigo(string pCodigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Usuario> ObtenerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
